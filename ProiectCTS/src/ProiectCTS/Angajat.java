14e12b1e13eca04d67781d54fa8b0eaf2b79dd01
Andy <andynistor2408@yahoo.com>
2015-05-16 15:14:31 +0300

    First commit

---
+++ ProiectCTS/src/ProiectCTS/Angajat.java
@@ -0,0 +1,13 @@
+package ProiectCTS;
+
+public class Angajat {
+	String nume;
+	String CNP;
+	String nrTelefon;
+	
+	public Angajat(String nume, String CNP, String nrTelefon) {
+		this.nume = nume;
+		this.CNP = CNP;
+		this.nrTelefon = nrTelefon;
+	}
+}
